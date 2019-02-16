9406b46ff18a90a8e43f242b611e49f1784a5ba4
SennaLyn <totatolin@foxmail.com>
2019-02-16 11:01:37 +0800

    LinkedList

--- Data-Structure/LinkedList.js
+++ Data-Structure/LinkedList.js
@@ -24,6 +24,30 @@ function LinkedList () {
 		}
 		length++;
 	}
+	// 从列表的特定位置移除一项
+	this.removeAt = function (position) {
+		// 检查越界值
+		if (position > -1 && position < length) {
+			let current = head,
+				previous,
+				index = 0;
+			// 移除第一项
+			if (position === 0) {
+				head = current.next;
+			} else {
+				while (index++ < position) {
+					previous = current;
+					current = current.next;
+				}
+				// 将previous与current的下一项链接起来：跳过current，从而移除它
+				previous.next = current.next;
+			}
+			length--;
+			return current.element;
+		} else {
+			return null;
+		}
+	}
 	// 获取链表数据
 	this.getHead = function () {
 		return head;
